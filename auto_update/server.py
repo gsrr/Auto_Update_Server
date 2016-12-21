59b98b5b2e81343a9fecb7194dbba072a8b5ee08
Jerry.Cheng <jerry.cheng@infortrend.com>
2016-12-21 16:28:31 +0800

    update

--- auto_update/server.py
+++ auto_update/server.py
@@ -3,6 +3,7 @@ from flask import request
 from flask_restful import Resource, Api, reqparse
 from flask import render_template
 import subprocess
+import os
 
 app = Flask(__name__)
 api = Api(app)
@@ -22,22 +23,34 @@ def write_config(nasip, module):
 def update_client():
 	subprocess.call("python update.py", shell=True)
 
-class HelloWorld(Resource):
-    def get(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('nasip', type=str)
-        parser.add_argument('module', type=str)
-        args = parser.parse_args()
-        print args
-        #write_config(request.remote_addr)
-        write_config(args['nasip'], args['module'])
-        update_client()
-        return {'hello': 'world'}
+@app.route('/update', methods=['POST'])
+def update():
+    #print request.form
+    parser = reqparse.RequestParser()
+    parser.add_argument('ip_dest', type=str)
+    parser.add_argument('module', type=str)
+    parser.add_argument('user_dest', type=str)
+    parser.add_argument('password_dest', type=str)
+    args = parser.parse_args()
+    print args
+    #write_config(request.remote_addr)
+    write_config(args['nasip'], args['module'])
+    update_client()
+    return {'hello': 'world'}
 
 #api.add_resource(HelloWorld, '/')
+def read_modules():
+    configs = os.listdir("./config")
+    return [ m.split(".")[1] for m in configs ]
+    
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    modules = read_modules()
+    return render_template(
+                    'index.html',
+                    modules = modules,
+           )
 
 if __name__ == '__main__':
    app.run(host="0.0.0.0")
